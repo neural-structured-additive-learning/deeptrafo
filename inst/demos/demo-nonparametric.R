e27852c78821b1850a336c4692f276af50040a17
lkook <lucasheinrich.kook@uzh.ch>
2022-03-29 15:56:01 +0200

    nonparm

---
+++ inst/demos/demo-nonparametric.R
@@ -0,0 +1,39 @@
+# Demo continuous response -- nonparametric estimator
+# Lucas Kook
+# Feb 2022
+
+set.seed(1234)
+
+# Deps --------------------------------------------------------------------
+
+library(tram)
+devtools::load_all("../deepregression/")
+devtools::load_all(".")
+
+# Data --------------------------------------------------------------------
+
+data("BostonHousing2", package = "mlbench")
+
+# Model -------------------------------------------------------------------
+
+tm <- Colr(cmedv ~ nox + chas, data = BostonHousing2, order = 25)
+
+BostonHousing2$ocmedv <- BostonHousing2$cmedv
+BostonHousing2$cmedv <- ordered(BostonHousing2$cmedv)
+
+## Unconditional
+m <- deeptrafo(cmedv ~ 1, data = BostonHousing2,
+               optimizer = optimizer_adam(learning_rate = 0.01, decay = 1e-4))
+fit(m, epochs = 4e3, validation_split = NULL, batch_size = nrow(BostonHousing2))
+
+plot(ecdf(BostonHousing2$ocmedv))
+lines(levels(BostonHousing2$cmedv), c(plogis(unlist(coef(m, which = "interacting")) + unlist(coef(m))), 1), type = "s", col = "red")
+
+## Conditional
+m <- deeptrafo(cmedv ~ 0 + chas + nox, data = BostonHousing2,
+               optimizer = optimizer_adam(learning_rate = 0.01, decay = 1e-4))
+fit(m, epochs = 8e3, validation_split = NULL, batch_size = nrow(BostonHousing2))
+
+plot(tm, which = "baseline only")
+lines(levels(BostonHousing2$cmedv)[-length(levels(BostonHousing2$cmedv))],
+      unlist(coef(m, which = "interacting")), type = "s", col = "red")
