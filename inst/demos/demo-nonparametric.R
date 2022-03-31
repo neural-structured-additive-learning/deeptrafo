d7d2fdb690066775ad7e9dfce9f2c70922048374
lkook <lucasheinrich.kook@uzh.ch>
2022-03-31 16:18:57 +0200

    nop

--- inst/demos/demo-nonparametric.R
+++ inst/demos/demo-nonparametric.R
@@ -16,25 +16,28 @@ data("BostonHousing2", package = "mlbench")
 
 # Model -------------------------------------------------------------------
 
-tm <- Colr(cmedv ~ nox + chas, data = BostonHousing2, order = 25)
+tm <- Colr(cmedv ~ nox + chas, data = BostonHousing2, order = 25,
+           support = range(BostonHousing2$cmedv))
 
 BostonHousing2$ocmedv <- BostonHousing2$cmedv
 BostonHousing2$cmedv <- ordered(BostonHousing2$cmedv)
 
-# otm <- Polr(cmedv ~ nox + chas, data = BostonHousing2) # takes long!
-# lvls <- as.numeric(levels(BostonHousing2$cmedv))
-# cfb <- coef(otm, with_baseline = TRUE)
-# plot(lvls[-length(lvls)], cfb[grepl("cmedv", names(cfb))], type = "s")
+if (FALSE) {
+  # otm <- Polr(cmedv ~ nox + chas, data = BostonHousing2) # takes long!
+  # lvls <- as.numeric(levels(BostonHousing2$cmedv))
+  # cfb <- coef(otm, with_baseline = TRUE)
+  # plot(lvls[-length(lvls)], cfb[grepl("cmedv", names(cfb))], type = "s")
 
-## Unconditional
-m <- deeptrafo(cmedv ~ 1, data = BostonHousing2,
-               optimizer = optimizer_adam(learning_rate = 0.1, decay = 1e-4))
-fit(m, epochs = 4e2, validation_split = NULL, batch_size = nrow(BostonHousing2))
+  ## Unconditional
+  m <- deeptrafo(cmedv ~ 1, data = BostonHousing2,
+                 optimizer = optimizer_adam(learning_rate = 0.1, decay = 1e-4))
+  fit(m, epochs = 4e2, validation_split = NULL, batch_size = nrow(BostonHousing2))
 
-plot(ecdf(BostonHousing2$ocmedv))
-lines(levels(BostonHousing2$cmedv),
-      c(plogis(unlist(coef(m, which = "interacting")) +
-                 unlist(coef(m))), 1), type = "s", col = "red")
+  plot(ecdf(BostonHousing2$ocmedv))
+  lines(levels(BostonHousing2$cmedv),
+        c(plogis(unlist(coef(m, which = "interacting")) +
+                   unlist(coef(m))), 1), type = "s", col = "red")
+}
 
 ## Conditional
 m <- deeptrafo(cmedv ~ 0 + chas + nox, data = BostonHousing2,
@@ -46,4 +49,3 @@ lines(levels(BostonHousing2$cmedv)[-length(levels(BostonHousing2$cmedv))],
       unlist(coef(m, which = "interacting")), type = "s", col = "red")
 legend("topleft", c("Bernstein 25", "Nonparametric"), col = c("black", "red"),
        lwd = 2)
-
