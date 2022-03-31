2f96d6cebd73d19dd87335afb01b7ae6a03683ad
lkook <lucasheinrich.kook@uzh.ch>
2022-03-31 16:20:16 +0200

    nop

--- inst/demos/demo-nonparametric.R
+++ inst/demos/demo-nonparametric.R
@@ -19,33 +19,33 @@ data("BostonHousing2", package = "mlbench")
 tm <- Colr(cmedv ~ nox + chas, data = BostonHousing2, order = 25,
            support = range(BostonHousing2$cmedv))
 
-BostonHousing2$ocmedv <- BostonHousing2$cmedv
-BostonHousing2$cmedv <- ordered(BostonHousing2$cmedv)
+BostonHousing2$cmedv <- BostonHousing2$cmedv
+BostonHousing2$ocmedv <- ordered(BostonHousing2$cmedv)
 
 if (FALSE) {
-  # otm <- Polr(cmedv ~ nox + chas, data = BostonHousing2) # takes long!
-  # lvls <- as.numeric(levels(BostonHousing2$cmedv))
+  # otm <- Polr(ocmedv ~ nox + chas, data = BostonHousing2) # takes long!
+  # lvls <- as.numeric(levels(BostonHousing2$ocmedv))
   # cfb <- coef(otm, with_baseline = TRUE)
   # plot(lvls[-length(lvls)], cfb[grepl("cmedv", names(cfb))], type = "s")
 
   ## Unconditional
-  m <- deeptrafo(cmedv ~ 1, data = BostonHousing2,
+  m <- deeptrafo(ocmedv ~ 1, data = BostonHousing2,
                  optimizer = optimizer_adam(learning_rate = 0.1, decay = 1e-4))
   fit(m, epochs = 4e2, validation_split = NULL, batch_size = nrow(BostonHousing2))
 
-  plot(ecdf(BostonHousing2$ocmedv))
-  lines(levels(BostonHousing2$cmedv),
+  plot(ecdf(BostonHousing2$cmedv))
+  lines(levels(BostonHousing2$ocmedv),
         c(plogis(unlist(coef(m, which = "interacting")) +
                    unlist(coef(m))), 1), type = "s", col = "red")
 }
 
 ## Conditional
-m <- deeptrafo(cmedv ~ 0 + chas + nox, data = BostonHousing2,
+m <- deeptrafo(ocmedv ~ 0 + chas + nox, data = BostonHousing2,
                optimizer = optimizer_adam(learning_rate = 0.1, decay = 1e-4))
 fit(m, epochs = 8e2, validation_split = NULL, batch_size = nrow(BostonHousing2))
 
 plot(tm, which = "baseline only")
-lines(levels(BostonHousing2$cmedv)[-length(levels(BostonHousing2$cmedv))],
+lines(levels(BostonHousing2$ocmedv)[-length(levels(BostonHousing2$ocmedv))],
       unlist(coef(m, which = "interacting")), type = "s", col = "red")
 legend("topleft", c("Bernstein 25", "Nonparametric"), col = c("black", "red"),
        lwd = 2)
