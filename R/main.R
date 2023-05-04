cc513e9078ffd160c2d01c094418ca1cd2a64466
Philipp Baumann <ph.baumann@t-online.de>
2023-05-04 23:23:34 +0200

    Vectorized CRPS learning with learning rates on BostonHousing

--- R/main.R
+++ R/main.R
@@ -522,18 +522,33 @@ crps <- function(base_distribution) {
   
   return(
     function(y_true, y_pred) {
+      
+      grid_size <- as.integer(grid_size)
+      
+      # for batch learning and validation, make sure the full grid of a obs is supplied
+      count_entries <- tf$unique_with_counts(tf$reshape(tf_stride_cols(y_true, 6L), -1L))
+      bool_mask <- tf$`repeat`(tf$equal(count_entries$count, grid_size), count_entries$count)
+      
+      # discard obs where only a part of the distribution is supplied
+      y_true <- tf$boolean_mask(y_true, bool_mask)
+      y_pred <- tf$boolean_mask(y_pred, bool_mask)
+      
+      batch_size <- as.integer(y_true$shape[1])
+      n_ID <- tf$cast(tf$divide(batch_size, grid_size), dtype = tf$int64)
 
-      #browser()
       # h_hat = h_1_hat + h_2_hat
       h_hat <- layer_add(list(tf_stride_cols(y_pred, 1L),
                               tf_stride_cols(y_pred, 2L)))
-      h_norm <- tf$norm(h_hat)
-      h_hat <- tf$divide(h_hat, h_norm)
       
-      # h'
+      # discuss: norm h_hat
+      h_hat <- tf$reshape(h_hat, c(n_ID, grid_size))
+      h_hat <- tf$map_fn(function(x) {
+        tf$divide(x[[1]], tf$norm(x[[1]]))
+        }, list(h_hat), dtype=tf$float32)
+      h_hat <- tf$reshape(h_hat, c(y_pred$shape[1], 1L))
+
+      # h_prime_hat
       h_prime <- tf$clip_by_value(tf_stride_cols(y_pred, 4L),1e-8, Inf)
-      h_norm <- tf$norm(h_prime)
-      h_prime <- tf$divide(h_prime, h_norm)
       h_prime <- tf$math$log(h_prime)
       
       # dont forget to add penalty to the loss
@@ -543,17 +558,11 @@ crps <- function(base_distribution) {
       par(mfrow = c(1,2))
       plot(f_y_dens$numpy()[1:grid_size])
       plot(f_y_dens$numpy()[(grid_size + 1):(grid_size * 2)])
-      #Sys.sleep(1)
-      #browser()
-      
-      grid_size <- as.integer(grid_size)
-      batch_size <- as.integer(f_y_dens$shape[1])
-      
-      n_ID <- tf$cast(tf$divide(batch_size, grid_size), dtype = tf$int64)
-      #if (n_ID < 32)  browser()
       
       y_grid <- tf$reshape(tf_stride_cols(y_true, 7L), c(n_ID, grid_size))
       f_y_hat <- tf$reshape(f_y_dens, c(n_ID, grid_size))
+      
+      # F_Y|X = x
       F_y_hat <- tf$map_fn(
         function(x) {
           scale_dens <- tfp$math$trapz(x[[1]], x[[2]])
@@ -563,13 +572,11 @@ crps <- function(base_distribution) {
         list(f_y_hat, y_grid),
         dtype=tf$float32
       )
-      #browser()
-      #F_y_hat <- tf$reshape(F_y_hat, c(-1L))
-      # #IDs <- tf$unique(tf$squeeze(tf_stride_cols(y_true, 6L), 1L))
-      # 
+
       M_crps <- 20L
       p_grid <- tf$linspace(0.01, 0.99, M_crps)
       
+      # interpolation of quantile function
       quantile_hat <- tf$map_fn(
         function(x) {
           lin_interpol(p_grid, x[[1]], x[[2]])
@@ -578,78 +585,23 @@ crps <- function(base_distribution) {
         dtype=tf$float32
       )
       
-      #browser()
       y_obs <- tf_stride_cols(y_true, 5L)
       y_obs <- tf$gather(y_obs, seq(1L, batch_size, grid_size))
       y_obs <- tf$`repeat`(y_obs, M_crps)
       
       quantile_hat <- tf$reshape(quantile_hat, -1L)
 
+      # quantile loss
       pin_ball <- tf$cast(tf$math$less(y_obs, quantile_hat),tf$float32)
       pin_ball <- tf$subtract(pin_ball, tf$tile(p_grid, 
                                                 list(n_ID)))
       pin_ball <- tf$math$multiply(pin_ball, tf$subtract(quantile_hat, y_obs))
       
+      # approximate crps
       crps <- tf$reduce_sum(tf$reshape(pin_ball, c(n_ID, M_crps)), axis = 1L)
-      
       scle <- tf$cast(tf$divide(2L, M_crps), tf$float32)
       crps <- tf$multiply(crps, scle)
-      
-      #browser()
-      #crps <- tf$reshape(tf$`repeat`(crps, grid_size), c(batch_size, 1L))
       crps <- tf$reduce_mean(crps)
-      
-
-      # 
-      # for (i in IDs$y$numpy()) {
-      #   
-      #   logical_idx <- tf$where(tf$squeeze(tf_stride_cols(y_true, 6L), 1L) == as.integer(i))
-      #   
-      #   f_y_dens_ID <- tf$reshape(tf$gather(f_y_dens, logical_idx), -1L)
-      #   y_grid_ID <- tf$reshape(tf$gather(tf_stride_cols(y_true, 7L), logical_idx), -1L)
-      #   
-      #   scale_dens <- tfp$math$trapz(f_y_dens_ID, y_grid_ID)
-      #   f_y_dens_ID <- tf$divide(f_y_dens_ID, scale_dens)
-      #   F_y_hat_ID <- comp_cdf(y_grid_ID, f_y_dens_ID)
-      #   
-      #   for (k in 1L:M_crps) {
-      #     
-      #     # find quantile through linear interpolation of quantile function
-      #     quant <- lin_interpol(p_grid[k], F_y_hat_ID, y_grid_ID)
-      #     
-      #     # pinball loss
-      #     pin_ball <- tf$cast(tf$math$less(y_obs, quant),tf$float32)
-      #     pin_ball <- tf$subtract(pin_ball, p_grid[k])
-      #     pin_ball <- tf$math$multiply(pin_ball, tf$subtract(quant, y_obs))
-      #     
-      #     pin_ball_loss <- tf$concat(list(pin_ball_loss, 
-      #                                     tf$reshape(pin_ball, 1L)), axis = 0L)
-      #   }
-      #   
-      #   scle <- tf$cast(tf$divide(2L, M_crps), tf$float32)
-      #   crps <- tf$math$multiply(scle, tf$reduce_sum(pin_ball_loss))
-      #   crp_scores <- tf$concat(list(crp_scores, 
-      #                                tf$reshape(crps, 1L)), axis = 0L)
-      # }
-      # 
-      # # F_Y|X = x
-      # # y_grid needs to be taken from "data"
-      # # 1-D integration should be done block wise (per ID)
-      # scale_dens <- tfp$math$trapz(f_y_dens, y_grid)
-      # f_y <- tf$divide(f_y_dens, scale_dens)
-      # F_y_hat <- tfp$math$trapz(f_y_dens, y_grid)
-      # 
-      # # 1-D interpolation should be done block wise (per ID)
-      # quant <- lin_interpol(F_y_hat, y_grid)
-      # 
-      # # block wise (per ID) pinball loss
-      # p_grid <- tf$linspace(0.01, 0.99, M_crps)
-      # pin_ball <- tf$cast(tf$math$less(y_true, quant),tf$float32)
-      # pin_ball <- tf$subtract(pin_ball, p_grid)
-      # pin_ball <- tf$math$multiply(pin_ball, tf$subtract(quant, y_true))
-      # 
-      # scle <- tf$cast(tf$divide(2L, M_crps), tf$float32)
-      # crps <- tf$math$multiply(scle, tf$reduce_sum(pin_ball)) # scalar
 
       return(crps)
     })
--- crps_loss.R
+++ crps_loss.R
@@ -1,25 +1,19 @@
 rm(list = ls())
-library(mlbench)
-library(data.table)
 reticulate::use_condaenv("/Users/flipst3r/opt/anaconda3/envs/r-reticulate", required = TRUE)
+
+library(mlbench)
+data("BostonHousing2")
 
 library(tensorflow)
 tf$config$run_functions_eagerly(TRUE) # does not construct a static comp graph to be executed later
 #tf$data$experimental$enable_debug_mode()
-
-# Prepare the data
-# data("wine", package = "ordinal")
-# wine$z <- rnorm(nrow(wine))
-# wine$x <- rnorm(nrow(wine))
-
-data("BostonHousing2")
-d <- setDT(BostonHousing2)
 
 # Set up neural network architecture
 nn <- \(x) x |>
   layer_dense(input_shape = 1L, units = 2L, activation = "relu") |>
   layer_dense(1L)
 
+# dev version
 devtools::load_all("/Users/flipst3r/RStHomeDir/GitHub/deeptrafo")
 
 # Model formula and definition
@@ -33,72 +27,17 @@ BostonHousing2 <- BostonHousing2 %>% tidyr::uncount(grid_size)
 BostonHousing2$y_grid <- rep(make_grid(BostonHousing2[["y_grid"]], n = grid_size)$y, n_size)
 BostonHousing2$ID <- rep(1:n_size, each = grid_size)
 
-# BostonHousing2 <- list(y_grid = list(rnorm(4), rnorm(4)), lon = list(rnorm(4), rnorm(4)),
-#                        age = list(rnorm(4), rnorm(4)), crim = list(rnorm(4), rnorm(4)))
-
-fml <- y_grid|lon ~ 0 + age + crim + lon + lat + ptratio
+fml <- y_grid ~ age + crim + lon + lat + ptratio + nn(nox)
 m <- deeptrafo(fml, BostonHousing2, latent_distr = "normal", monitor_metric = NULL,
-               optimizer = optimizer_adam(learning_rate = 0.0001),
+               optimizer = optimizer_adam(learning_rate = 0.1),
                return_data = TRUE, list_of_deep_models = list(nn = nn), crps = TRUE,
                trafo_options = trafo_control(support = c(5, 50)))
 
-# n <- nrow(BostonHousing2)
-# not_found <- TRUE
-# i <- 101
-# while(not_found) {
-#   if (i %% 50L == 0) {
-#     candidate = i / 50L
-#     if (n %% candidate == 0) {
-#       print(i)
-#       not_found <- FALSE
-#     }
-#   }
-#   i <- i + 1
-# }
-# # Overview
-# print(m)
-
-
-# Fit
-m %>% fit(epochs = 20,
-          batch_size = 64*grid_size, 
-          #shuffle = FALSE,
-          validation_data = NULL,
-          validation_split = list())
-
-# Fit model using custom generator
-batch_size <- 10
-steps_per_epoch <- ceiling(length(y) / batch_size)
-model %>% fit_generator(
-  generator = custom_generator(x, y, id, batch_size),
-  steps_per_epoch = steps_per_epoch,
-  epochs = 10
-)
-
-fit_generator()
-
-list(a =  , b =)
-
-data.frame()
-
-# Prepare the data
-data("wine", package = "ordinal")
-wine$z <- rnorm(nrow(wine))
-wine$x <- rnorm(nrow(wine))
-
-# Set up neural network architecture
-nn <- \(x) x |>
-  layer_dense(input_shape = 1L, units = 2L, activation = "relu") |>
-  layer_dense(1L)
-
-options(tensorflow.extract.disallow_out_of_bounds = FALSE)
-# Model formula and definition
-fml <- rating ~ 0 + temp + contact + s(z, df = 3) + nn(x)
-m <- deeptrafo(fml, wine, latent_distr = "logistic", monitor_metric = NULL,
-               return_data = TRUE, list_of_deep_models = list(nn = nn))
-
-# Overview
-print(m)
-
-# Fit
-m %>% fit(epochs = 10, batch_size = nrow(wine))
+m %>% fit(epochs = 200L,
+          batch_size = 64*grid_size,
+          callbacks = list(callback_early_stopping(patience = 5, 
+                                                   monitor = "val_loss",
+                                                   restore_best_weights = T),
+                           callback_reduce_lr_on_plateau(patience = 2, factor = 0.5)),
+          validation_split = 0.2,
+          shuffle = FALSE)
