c2085db5a65c365b7ff8f3d7fcd14b97e87d595d
Lucas Kook <lucasheinrich.kook@gmail.com>
2024-12-02 22:52:43 +0100

    rm tf import

--- R/callbacks.R
+++ R/callbacks.R
@@ -1,12 +1,10 @@
 ################################################################################################
 
-#' @importFrom tensorflow tf
 #' @importFrom tfprobability tfd_normal tfd_logistic tfd_log_prob tfd_gumbel
-tf_crps_norm <- function(h)
-{
+tf_crps_norm <- function(h) {
   return(
-    h * (2 * tfd_normal(0,1)$cdf(h) - 1) +
-      (tf$math$sqrt(2) * tf$math$exp(-0.5 * tf$square(h)) - 1)/tf$math$sqrt(pi)
+    h * (2 * tfd_normal(0, 1)$cdf(h) - 1) +
+      (tf$math$sqrt(2) * tf$math$exp(-0.5 * tf$square(h)) - 1) / tf$math$sqrt(pi)
   )
 }
 
@@ -15,11 +13,9 @@ tf_crps_norm <- function(h)
 #' @importFrom  keras custom_metric
 #'
 crps_stdnorm_metric <- custom_metric(name = "crps_stdnorm", metric_fn = function(y_true, y_pred) {
-
-  h_y_eval <- y_pred[,1, drop = FALSE] + y_pred[,2, drop = FALSE]
+  h_y_eval <- y_pred[, 1, drop = FALSE] + y_pred[, 2, drop = FALSE]
 
   return(tf_crps_norm(h_y_eval))
-
 })
 
 ################################################################################################
